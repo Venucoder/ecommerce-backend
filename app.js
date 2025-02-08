507daf4d7c94236060b0b9dbe60b981736d3da6c
Venugopal-VL <venugopal.kallam@vassarlabs.com>
2025-02-08 14:46:37 +0530

    Initial setup

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Database connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('E-Commerce Backend is running!');
+});
+
+module.exports = app;
